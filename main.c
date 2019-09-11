32a15fd7e8b06b92fe003132d26c41cc373c7423
Brod8362 <brod8362@gmail.com>
2019-09-11 12:05:15 -0500

    blank window

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <gtk/gtk.h>
-
+#include <stdio.h>
 static void activate(GtkApplication* app, gpointer data) {
+	GtkWidget* window;
+	printf("Initializing mpdX\n");
 	
+	window = gtk_application_window_new(app);
+	gtk_window_set_title(GTK_WINDOW(window), "mpdX");
+	gtk_window_set_default_size(GTK_WINDOW(window), 825, 400);
+
+	gtk_widget_show_all(window);
 }
 
 int main(int argc, char *argv[]) {
