a9e9c35f26f5b0175e05d6ba5500315ccacd5268
Brod8362 <brod8362@gmail.com>
2019-09-11 12:23:41 -0500

    basic menubar

--- main.c
+++ main.c
@@ -1,12 +1,28 @@
 #include <gtk/gtk.h>
 #include <stdio.h>
+
+static void initialize_menu_bar(GtkApplication* app) {
+	GtkBuilder* builder;
+	GMenuModel* app_menu;
+	GResource* resources;
+
+	builder = gtk_builder_new_from_resource("/byakuren/resources/menubar.xml");
+	app_menu = G_MENU_MODEL(gtk_builder_get_object(builder, "appmenu"));
+	gtk_application_set_app_menu(app, app_menu);
+	g_object_unref(builder); //free memory used by builder
+}
+
 static void activate(GtkApplication* app, gpointer data) {
 	GtkWidget* window;
 	printf("Initializing mpdX\n");
-	
+
+	/* init base window */	
 	window = gtk_application_window_new(app);
 	gtk_window_set_title(GTK_WINDOW(window), "mpdX");
 	gtk_window_set_default_size(GTK_WINDOW(window), 825, 400);
+
+	/* init menubar */
+	initialize_menu_bar(app);
 
 	gtk_widget_show_all(window);
 }
--- makefile
+++ makefile
@@ -1,2 +1,9 @@
 main:
-	gcc $$(pkg-config gtk+-3.0 --cflags) main.c -o main $$(pkg-config gtk+-3.0 --libs) -g3
+	mkdir .tmp
+	glib-compile-resources --target=.tmp/resources.c --generate-source resources/window.xml
+	gcc $$(pkg-config gtk+-3.0 --cflags) main.c .tmp/resources.c -o main $$(pkg-config gtk+-3.0 --libs) -g3
+	rm -rf .tmp
+
+clean:
+	rm -rf .tmp
+	rm main
