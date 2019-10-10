87ccbf0148bda2922aee3c5f30f81fe344aadaef
Brod8362 <brod8362@gmail.com>
2019-10-09 20:18:57 -0500

    song search c&h

---
+++ song_search_dialog.c
@@ -0,0 +1,6 @@
+#include <mpd/client.h>
+#include <gtk/gtk.h>
+
+#include "song_search_dialog.h"
+#include "main.h"
+#include "mpd_actions.h"
---
+++ song_search_dialog.h
@@ -0,0 +1,6 @@
+#ifndef INCLUDED_SONG_SEARCH_H
+#define INCLUDED_SONG_SEARCH_H
+
+
+
+#endif
